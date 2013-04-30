61a26f3b9d659be8de14128f27a6377fb1cf6895
Jason Howmans <jasonhowmans@me.com>
2013-04-30 22:44:24 +0100

    Basics

---
+++ js/lib/slizzle.js
@@ -0,0 +1,9 @@
+/**!
+  S L I Z Z L E
+  Slizzle CSS slideshow enhancer.
+  - - - - - - - - - - - - - - - - -
+  @created April 2013
+  @by Jason Howmans (@jasonhowmans).
+  @dependencies jQuery 1.9
+ **/
+
