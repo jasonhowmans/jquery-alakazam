bb87474db6279db4879f89a58f35e768c0a447e5
Jason Howmans <jasonhowmans@me.com>
2013-05-01 09:47:04 +0100

    Plugin and demo bases laid.

---
+++ js/lib/jquery.slizzle.js
@@ -0,0 +1,30 @@
+/**!
+  S L I Z Z L E
+  Slizzle CSS slideshow enhancer.
+  - - - - - - - - - - - - - - - - -
+  @created April 2013
+  @by Jason Howmans (@jasonhowmans).
+  @dependencies jQuery 1.9.1
+ **/
+
+;(function($) {
+
+	var do = {
+
+	};
+
+	$.fn.slizzle = function( options ) {
+		var $this = this;
+
+		// Create default settings object and extend with passed options.
+		var settings = $.extend({
+			enterClass: 'in',
+			exitClass: 'out',
+			slidePause: 2000
+
+		}, options);
+
+		
+
+	};
+})( jQuery );
--- js/lib/slizzle.js
+++
@@ -1,9 +0,0 @@
-/**!
-  S L I Z Z L E
-  Slizzle CSS slideshow enhancer.
-  - - - - - - - - - - - - - - - - -
-  @created April 2013
-  @by Jason Howmans (@jasonhowmans).
-  @dependencies jQuery 1.9
- **/
-
